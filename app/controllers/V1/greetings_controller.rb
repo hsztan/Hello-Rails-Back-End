fa636519b861710e01b3ac9566d128305b577a12
Henry Nawrocki <hdev@sztanski.com>
2022-08-09 11:24:57 -0500

    Add index action in GreetingsController

--- app/controllers/V1/greetings_controller.rb
+++ app/controllers/V1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class V1::GreetingsController < ApplicationController
   def index
-    render json: { greeting: 'Hello World' }
+    render json: { greeting: Greeting.all.sample.greeting }
   end
 end
